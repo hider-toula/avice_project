bb42e0e0630e88ac6d4e8f8769d184d73e5b5f4c
hider-toula <hyder.toula@gmail.com>
2023-01-04 12:04:13 +0100

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from . import views # import views from the same directory
 
-from . import views
+
 
 urlpatterns = [
+    
+    
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('actu/', views.actu, name='actu'),
+    path('actu', views.actu, name='actu'),
+    path('<int:project_id>', views.detail, name='detail'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,29 +1,62 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
+from .models import Project, Actu
 
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
-
     
-def about(request):
-    return render(request, 'pages/about.html')
+    projects = Project.objects.all().order_by('classement')
+    
+    
+    context = {
+        
+        'projects': projects,
 
+    }
+    
+    return render(request, 'pages/index.html',context)
+
+
+def about(request):
+    
+    
+    actus = About.objects.all()
+    
+    context = {
+        'about': about,
+    }
+    
+    return render(request, 'pages/about.html', context)
 
 
 def contact(request):
     return render(request, 'pages/contact.html')
 
 
-
 def actu(request):
-    return render(request, 'pages/actu.html')
+    
+    actus = Actu.objects.all()
+    
+    context = {
+        
+        'actus': actus,
+
+        }
+    
+    return render(request, 'pages/actu.html', context)
 
 
 
- 
-
-
-
-
-
+def detail(request, project_id):
+    
+    project = get_object_or_404(Project, pk=project_id)
+    
+    context = {
+        'project': project,
+    }
+    
+    return render(request, 'pages/project.html', context)
