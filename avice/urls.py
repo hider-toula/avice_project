3b2f8296a33e9da3496d979953b0c616d396a626
hider-toula <hyder.toula@gmail.com>
2023-01-04 10:22:31 +0100

    initial commit

--- avice/urls.py
+++ avice/urls.py
@@ -1,3 +1,4 @@
+
 """avice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
 
 urlpatterns = [
+    path( '', include('pages.urls')),
     path('admin/', admin.site.urls),
+    
 ]
