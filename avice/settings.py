06fcb87406a5c746cf496efce3549695cd29b2ec
hider-toula <hyder.toula@gmail.com>
2023-01-19 10:59:12 +0100

    added settings

--- avice/settings.py
+++ avice/settings.py
@@ -133,11 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'contact@avice.fr'
-EMAIL_HOST_PASSWORD = ''
-EMIAL_USE_TLS = True
 
 try:
     from .local_settings import *
